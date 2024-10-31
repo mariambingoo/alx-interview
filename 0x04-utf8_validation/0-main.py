f568b97366b37c59c09a1c950afa27009bec56e6
mariambingoo <mariamsheta13@gmail.com>
2024-11-01 00:42:30 +0200

    answer

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -1,10 +1,19 @@
-validUTF8 = __import__('0-validate_utf8').validUTF8
+#!/usr/bin/python3
+"""
+Testing UTF-8 Validation
+"""
 
+is_valid_utf8 = __import__('0-validate_utf8').is_valid_utf8
+
+# Testing single-byte ASCII characters
 data = [65]
-print(validUTF8(data))
+print(is_valid_utf8(data))  # Expected output: True
 
+# Testing a sequence of ASCII characters
 data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-print(validUTF8(data))
+print(is_valid_utf8(data))  # Expected output: True
 
+# Testing a sequence with an invalid byte
 data = [229, 65, 127, 256]
-print(validUTF8(data))
+print(is_valid_utf8(data))  # Expected output: False
+
