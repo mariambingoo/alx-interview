ccdfc2d70d8d33e423143688f3b301b10ce52e6e
mariambingoo <mariamsheta13@gmail.com>
2024-12-06 20:32:12 +0200

    0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-#([1, 2, 25], 37))
+""" 0-making_change"""
 
 def makeChange(coins, total):
-  coins.sort() # [1 ,  2 , 25]
+  coins.sort() 
   sum = 0
   index = 1
   while total:
 
-    if total < coins[-1 * index]: # 12 < 2
+    if total < coins[-1 * index]: 
       index = index + 1
       if (total < 0):
         return -1
     if (total == 0):
       break
     else:
-      total = total - coins[-1*index]  #12 = 12 -2
+      total = total - coins[-1*index] 
       sum = sum + 1
   return sum
-
-print(makeChange([1, 2, 25], 37))
