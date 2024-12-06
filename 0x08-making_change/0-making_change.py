bdad5b4e2666ca30e1cca5332af39f22992c58e9
mariambingoo <mariamsheta13@gmail.com>
2024-12-06 20:35:15 +0200

    0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
-""" 0-making_change"""
+"""
+0-making_change.py: A script to determine the minimum number of coins
+needed to make change for a given amount.
+"""
+
 
 def makeChange(coins, total):
-  coins.sort() 
-  sum = 0
-  index = 1
-  while total:
+    """
+    Determines the minimum number of coins needed to make change for `total`.
 
-    if total < coins[-1 * index]: 
-      index = index + 1
-      if (total < 0):
-        return -1
-    if (total == 0):
-      break
-    else:
-      total = total - coins[-1*index] 
-      sum = sum + 1
-  return sum
+    Args:
+        coins (list): A list of coin denominations.
+        total (int): The total amount of change required.
+
+    Returns:
+        int: The minimum number of coins needed to make the change.
+             Returns -1 if the change cannot be made.
+    """
+    if total <= 0:
+        return 0  # No coins needed if total is 0 or negative
+
+    coins.sort(reverse=True)  # Sort coins in descending order
+    coin_count = 0
+    index = 0
+
+    while total > 0:
+        if index >= len(coins):  # No coins left to try
+            return -1
+
+        if total >= coins[index]:  # Use the current coin if it fits
+            total -= coins[index]
+            coin_count += 1
+        else:
+            index += 1  # Move to the next smaller coin
+
+    return coin_count
